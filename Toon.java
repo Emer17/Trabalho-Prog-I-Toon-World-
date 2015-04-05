025194fab57c615c4606da664c7f571cd585aeb8
Emer17 <emer-martins@hotmail.com>
2015-04-05 17:06:18 -0300

    Adicionado UML e Toon.java

---
+++ Toon.java
@@ -0,0 +1,19 @@
+/* Nome : Toon.java (representa Main.java)
+ * Autores: Emerson Martins  <emer-martins@hotmail.com>
+ * 			Leonardo Vargas  <leu1607@hotmail.com>
+ * Versão: 1.0
+ * Descrição: Classe Main da Toon World, linguagem baseada em java.*/
+ 
+import java.util.Scanner;
+import java.io.*;
+
+class Toon {
+    public static void main(String args[]) throws Exception {
+		file f;
+		Scanner s;
+		//fazer interpretador
+		
+		
+		
+		
+	}
