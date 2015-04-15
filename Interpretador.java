b7238444f40405f138c1e599abfbf72d111ee5a9
Leonardo Varga <leu1607@hotmail.com>
2015-04-15 12:00:12 -0300

    Foi feita a concatenação de String na class Operacao.java

--- Interpretador.java
+++ Interpretador.java
@@ -10,8 +10,6 @@ class Interpretador{
 	public Doublee vetor2[] = new Doublee[1000];
 	public Stringg vetor3[] = new Stringg[1000];
 
-	
-	
 	public void corrige(String l[]){
 		this.linhas = l; //este bloco corrige o problema de "espaços duplicados"
 		linhas = l;
@@ -38,94 +36,54 @@ class Interpretador{
 		if(linha.contains("main")){
 			return;
 		}else if(linha.contains("int") || linha.contains("double") || linha.contains("string")){
-				for(i=0;i<linha.length();i++){
-					if(linha.charAt(i) ==' ' && linha.charAt(i-1) == 't' || linha.charAt(i) ==' ' && linha.charAt(i-1) == 'e' || linha.charAt(i) ==' ' && linha.charAt(i-1) == 'g'){
-						for(k=i,j=0;j<(linha.length()-i);k++){
-							if(copia[k]==' ' || copia[k]== '?'){
-								continue;
-							}else if(copia[k]=='<'){
-								k = k+2;
-								if(copia[k]==' '){
-									k++;
-								}
-								while(copia[k] != ' '){
-									valor[x] = copia[k];
-									k++;
-									x++;
-									valor2 = String.copyValueOf(valor);
-									m = 1 * Double.valueOf(valor2).doubleValue();
-									//System.out.println(m);
-								}
-							}else{
-								nome[j] = copia[k];
-								j++;
-								nome2 = String.copyValueOf(nome);
-								//System.out.println(nome2);
+			for(i=0;i<linha.length();i++){
+				if(linha.charAt(i) ==' ' && linha.charAt(i-1) == 't' || linha.charAt(i) ==' ' && linha.charAt(i-1) == 'e' || linha.charAt(i) ==' ' && linha.charAt(i-1) == 'g'){
+					for(k=i,j=0;j<(linha.length()-i);k++){
+						if(copia[k]==' ' || copia[k]== '?'){
+							continue;
+						}else if(copia[k]=='<'){
+							k = k+2;
+							if(copia[k]==' '){
+								k++;
 							}
+							while(copia[k] != ' '){
+								valor[x] = copia[k];
+								k++;
+								x++;
+								valor2 = String.copyValueOf(valor);
+								m = 1 * Double.valueOf(valor2).doubleValue();
+								//System.out.println(m);
+							}
+						}else{
+							nome[j] = copia[k];
+							j++;
+							nome2 = String.copyValueOf(nome);
+							//System.out.println(nome2);
 						}
 					}
 				}
 			}
 		}
-
-		
-	public double teste_k(double k){
-		if(k == 1){
-			k = 10;
-		} else {
-			k = k*10;
-		}
-		return k;
 	}
 	
     public void interpreta(String l[]) {
         this.linhas = l;
-		
 		for(int i = 0; i < this.linhas.length; i++) {
             if(this.linhas[i] != null) {
+				boolean Token_Especial = false;
 				for(int x = 0; x < linhas[i].length(); x++ ){
-					if(linhas[i].charAt(x) == '-' || linhas[i].charAt(x) == '+' || linhas[i].charAt(x) == '/' || linhas[i].charAt(x) == '*' || linhas[i].charAt(x) == '%'){
-						Operacao OP = new Operacao();
-						Boolean Percorre_W = true;
-						int w = x-1;
-						double k = 1;
-						while(linhas[i].charAt(w) != ' '){
-							OP.setA(linhas[i].charAt(w),k);
-							k = teste_k(k);
-							w--;
-						}
-						w = x+1;
-						k = 1;
-						while(linhas[i].charAt(w) != '?'){
-							w++;
-						}
-						w--;
-						while(Percorre_W){
-							if(linhas[i].charAt(w) == '-' || linhas[i].charAt(w) == '+' || linhas[i].charAt(w) == '/' || linhas[i].charAt(w) == '*' || linhas[i].charAt(w) == '%'){
-								Percorre_W = false;
-							} else {
-								OP.setB(linhas[i].charAt(w),k);
-								k = teste_k(k);
-								w--;
-							}
-						}
-						if (linhas[i].charAt(x) == '-'){
-							OP.Sub();
-						} else if (linhas[i].charAt(x) == '+') {
-							OP.Soma();
-						} else if (linhas[i].charAt(x) == '/') {
-							OP.Div();
-						} else if (linhas[i].charAt(x) == '*') {
-							OP.Mult();
-						} else if (linhas[i].charAt(x) == '%') {
-							OP.Mod();
-						}
-						System.out.println("Oh resultado da op eh: " + OP.Result);
-					}	
+					Operacao OP = new Operacao();
+					if(OP.TokenEspecial(linhas[i].charAt(x))){
+						Token_Especial = true;
+					}
+					if(OP.testeExpressao(linhas[i].charAt(x))){
+						char Token = linhas[i].charAt(x);
+						OP.Exp(linhas[i], Token, Token_Especial);
+						OP.Imp(Token);
+					}
 				}
-            }
-        }
-		
+			}
+		}
         /*for(int i = 0; i < this.linhas.length; i++) {
             int x = 0;
             if(this.linhas[i] != null) {
--- Operacao.java
+++ Operacao.java
@@ -1,36 +1,115 @@
 class Operacao{
-	public double A;
-	public double B;
-	public double Result;
+	private double A;
+	private double B;
+	private double Result;
 	
 	public Operacao(){
 		this.A = 0;
 		this.B = 0;
 		this.Result = 0;
 	}
-	public void setA(char a, double k){
-		String a2 = ""+ a;
-		k = k * Double.valueOf(a2).doubleValue();
-		this.A =  this.A + k;
+	
+	public void Imp(char Token){
+		System.out.println("Oh resultado de " + Math.round(this.A) + " " + Token + " " + Math.round(this.B) +" eh: " + this.Result);
 	}
-	public void setB(char b, double k){
-		String b2 = ""+ b;
-		k = k * Double.valueOf(b2).doubleValue();
-		this.B =  this.B + k;
+	
+	public void setA(String C, boolean T_E){
+		if (T_E) this.A = Double.valueOf(C).doubleValue()*(-1);
+		else this.A = Double.valueOf(C).doubleValue();
 	}
+	
+	public void setB(String C){
+		this.B = Double.valueOf(C).doubleValue();
+	}
+	
 	public void Soma(){
 		this.Result = this.A + this.B;
 	}
+	
 	public void Sub(){
 		this.Result = this.A - this.B;
 	}
+	
 	public void Mult(){
 		this.Result = this.A * this.B;
 	}
+	
 	public void Div(){
 		this.Result = this.A / this.B;
 	}
+	
 	public void Mod(){
 		this.Result = this.A % this.B;
+	}
+	
+	public boolean testeExpressao(char Token){
+		boolean T;
+		if(Token == '-'){ 
+			T = true;
+		} else if (Token == '+'){ 
+			T = true;
+		} else if (Token == '/'){ 
+			T = true;	
+		} else if (Token == '*'){ 
+			T = true;
+		} else if (Token == '%'){
+			T = true;
+		} else {
+			T = false;
+		}
+		return T;
+	}
+	
+	public boolean TokenEspecial(char T_Especial){
+		boolean T;
+		if(T_Especial == '@'){ 
+			T = true;
+		} else {
+			T = false;
+		}
+		return T;
+	}
+	
+	public void Exp(String l, char Token, boolean T_E){
+		int x;
+		for(x = 0; x < l.length(); x++ ){
+			if(testeExpressao(l.charAt(x))){
+				break;
+			}
+		}
+		int w = x-1;
+		String Concatenar = "";
+		while(l.charAt(w) != ' '){
+			Concatenar += l.charAt(w);
+			w--;
+		}
+		Concatenar = Inverter(Concatenar);
+		setA(Concatenar, T_E);
+		Concatenar = "";
+		w = x+1;
+		while(l.charAt(w) != '?'){
+			Concatenar += l.charAt(w);
+			w++;
+		}
+		setB(Concatenar);
+		if (Token == '-'){
+			Sub();
+		} else if (Token == '+') {
+			Soma();
+		} else if (Token == '/') {
+			Div();
+		} else if (Token == '*') {
+			Mult();
+		} else if (Token == '%') {
+			Mod();
+		}
+	}
+	
+	public String Inverter(String S){  
+        String Reverse = "";
+		for (int w = S.length() - 1; w > -1; w--) {    
+            Reverse += String.valueOf(S.charAt(w));
+		}
+		return Reverse;
 	}
 }
